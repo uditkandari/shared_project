9edec8ea998f2ff5353a538725701d8446d80151
uditkandari <udit.kandari@gmail.com>
2021-02-11 11:29:47 +0530

    html task

---
+++ gitPract/src/com/gitPract/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.gitPract;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three no: ");
+		int number = sc.nextInt();
+		
+	}
+
+}
